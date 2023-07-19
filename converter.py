4d3cada150300442435754f8d352fedd8d1f41c2
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-07-19 13:33:42 -0700

    docs

---
+++ converter.py
@@ -0,0 +1,11 @@
+"""
+.NDM2 Converter to Django models.py
+
+Step 1: Go through the schema and make a list of each:
+    a. Entity Table
+    b. attribute
+    c. relationship
+Step 2: Save the lists to a sqlite3 database (for now) each in thier own table
+Step 3: write the models.py file(s) based on the sqlite3 database
+"""
+
